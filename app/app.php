fd9700a5d0a6387cbcde310b1d0e96a0ea5a163e
Ronando Long <ronando.long@gmail.com>
2017-01-24 12:41:35 -0800

    take in user's text input from browser in form of a task and display the description of the task along with all previous tasks using supervariables _SESSION and _POST

--- app/app.php
+++ app/app.php
@@ -2,28 +2,56 @@
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/task.php";
-                                                // The first line starts our session Then checks the session for the
-    // session_start();                            // key 'list_of_tasks' using the EMPTY function, which returns true if there
-    // if (empty($_SESSION['list_of_tasks'])) {    // is no value stored in the variable
-    //     $_SESSION['list_of_tasks'] = array();   // If the key doesn't exist, we create it and set it to an empty array
-    // }                                           // $_SESSION is a superglobal variable like $_GET
-                                                // superglobal means we can access it from anywhere in our code.
+
+    // The first line starts our session Then checks the session for the key 'list_of_tasks' using the EMPTY function
+    session_start();                            // which returns true if there key 'list_of_tasks' using the EMPTY function
+    if (empty($_SESSION['list_of_tasks'])) {    // If the key doesn't exist, we create it and set it to an empty array
+        $_SESSION['list_of_tasks'] = array();   // $_SESSION is a superglobal variable like $_GET
+    }                                           // superglobal means we can access it from anywhere in our code
+                                                // Storing the objects in cookies on the users' browser
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        $task1 = new Task("Learn PHP");
-        $task2 = new Task("Learn WordPress");
-        $task3 = new Task("Learn Python");
 
-        $tasks = [$task1, $task2, $task3];
         $output = "";
+                      // We only want to list tasks if some exist.
+        $all_tasks = Task::getAll();
 
-        foreach ($tasks as $task) {
-          $output = $output . "<p>" . $task->getDescription() . "</p>";
+        if (!empty($all_tasks)) {
+          $output = $output . "
+              <h1>To Do List</h1>
+              <p>Here are all your tasks</p>
+              ";
+          foreach ($all_tasks as $task) {
+            $output = $output . "<p>" . $task->getDescription() . "</p>";
         }
+
+        }
+
+        $output = $output . "
+            <form action='/tasks' method='post'>
+                <label for='description'>Task Description</label>
+                <input type='text' id='description' name='description'>
+
+                <button type='submit'>Add task</button>
+            </form>
+            ";
 
         return $output;
     });
 
+    $app->post("/tasks", function() {
+        $task = new Task($_POST['description']);
+        $task->save();
+
+        return "
+            <h1>You created a new task</h1>
+            <p>" . $task->getDescription() . "</p>
+            <p><a href='/'>View your list of things to do.</a></p>
+        ";
+    });
+
     return $app;
+
+
 ?>
--- src/task.php
+++ src/task.php
@@ -8,7 +8,7 @@
             $this->description = $task_description;
         }
 
-
+        // Setter and Getter
         function setDescription($new_description)
         {
             $this->description = $new_description;
@@ -18,6 +18,19 @@
         {
             return $this->description;
         }
+
+        // We store a Task by calling a save method on it
+        function save()
+        {                                      // 'list_of_tasks' is a key for k/v pair
+            array_push($_SESSION['list_of_tasks'], $this);
+        }                                      // we're pushing whatever we call the save() method on into the array 'list_of_tasks'
+
+// 'Static Method'is a getter, but works on the whole class: it returns the list of all our tasks.
+        static function getAll()
+        {
+            return $_SESSION["list_of_tasks"];  // Returns a list of all the tasks
+        }
+
 
     }
 ?>
